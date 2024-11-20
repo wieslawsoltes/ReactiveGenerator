1a27954f3d6cd19ef2cfaf1c98d404a625918221
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-11-20 10:51:09 +0100

    Update Person.cs

--- ReactiveGeneratorDemo/ViewModels/Person.cs
+++ ReactiveGeneratorDemo/ViewModels/Person.cs
@@ -3,14 +3,14 @@ namespace ReactiveGeneratorDemo.ViewModels;
 public partial class Person
 {
     [Reactive]
-    public partial string FirstName { get; set; }
+    public partial string? FirstName { get; set; }
 
     [Reactive]
-    public partial string LastName { get; set; }
+    public partial string? LastName { get; set; }
 
     [Reactive]
     public partial int Age { get; set; }
 
     [Reactive] 
-    internal partial string Tag { get; private set; }
+    internal partial string? Tag { get; private set; }
 }
