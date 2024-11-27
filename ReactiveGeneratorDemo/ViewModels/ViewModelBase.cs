69944f90512ff6ae582b910337081f8cd756be3c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-11-27 20:31:16 +0100

    Update ViewModelBase.cs

--- ReactiveGeneratorDemo/ViewModels/ViewModelBase.cs
+++ ReactiveGeneratorDemo/ViewModels/ViewModelBase.cs
@@ -3,4 +3,13 @@ namespace ReactiveGeneratorDemo.ViewModels;
 [Reactive]
 public partial class ViewModelBase
 {
+    [Reactive]
+    public partial bool IsInitialized { get; set; }
+}
+
+[Reactive]
+public partial class MainViewModel : ViewModelBase
+{
+    [Reactive]
+    public partial object? Data { get; set; }
 }
