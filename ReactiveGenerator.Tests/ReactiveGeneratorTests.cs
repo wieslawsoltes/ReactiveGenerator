897694d0d9486cf481911dbb996772290384055e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-12-12 10:43:45 +0100

    Update ReactiveGeneratorTests.cs

--- ReactiveGenerator.Tests/ReactiveGeneratorTests.cs
+++ ReactiveGenerator.Tests/ReactiveGeneratorTests.cs
@@ -204,7 +204,284 @@ public class ReactiveGeneratorTests
 
         return TestAndVerify(source);
     }
+    
+    [Fact]
+    public Task ClassWithMultipleReactiveAttributes()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                [Reactive]
+                public partial string DoubleDeclared { get; set; }
+            }";
 
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithCustomPropertyImplementationAndReactiveAttribute()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                private string _customImpl = string.Empty;
+                [Reactive]
+                public partial string CustomImpl 
+                { 
+                    get => _customImpl;
+                    set => _customImpl = value;
+                }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithExplicitInterfaceImplementation()
+    {
+        var source = @"
+            public interface IHasName
+            {
+                string Name { get; set; }
+            }
+
+            [Reactive]
+            public partial class TestClass : IHasName
+            {
+                string IHasName.Name { get; set; }
+                public partial string PublicName { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task NestedClassWithInheritance()
+    {
+        var source = @"
+            public partial class OuterClass
+            {
+                [Reactive]
+                public partial class InnerBase
+                {
+                    public partial string BaseProp { get; set; }
+                }
+
+                [Reactive]
+                public partial class InnerDerived : InnerBase
+                {
+                    public partial string DerivedProp { get; set; }
+                }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithGenericConstraintsAndNullableProperties()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass<T, U> 
+                where T : class?, IDisposable
+                where U : struct, IComparable<U>
+            {
+                public partial T? NullableRef { get; set; }
+                public partial U? NullableStruct { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithAbstractProperties()
+    {
+        var source = @"
+            [Reactive]
+            public abstract partial class TestClass
+            {
+                public abstract string AbstractProp { get; set; }
+                public partial string ConcreteProp { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithPropertyAccessorModifiers()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                public partial string PropWithPrivateSet { get; private set; }
+                protected partial string PropWithProtectedGet { private get; set; }
+                internal partial string PropWithInternalSet { get; internal set; }
+                public partial string PropWithProtectedInternalSet { get; protected internal set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithVirtualProperties()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                public virtual partial string VirtualProp { get; set; }
+                public partial string NonVirtualProp { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithStaticProperties()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                public static partial string StaticProp { get; set; }
+                public partial string InstanceProp { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithIndexers()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                private string[] _items = new string[10];
+                
+                public string this[int index]
+                {
+                    get => _items[index];
+                    set => _items[index] = value;
+                }
+
+                public partial string RegularProp { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithNestedGenerics()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass<T>
+                where T : class
+            {
+                public partial List<Dictionary<string, T>> ComplexProp { get; set; }
+                public partial Dictionary<int, List<T?>> NestedProp { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithMultipleInheritanceLevels()
+    {
+        var source = @"
+            public partial class GrandParent
+            {
+                public virtual string GrandParentProp { get; set; }
+            }
+
+            [Reactive]
+            public partial class Parent : GrandParent
+            {
+                public partial string ParentProp { get; set; }
+            }
+
+            [Reactive]
+            public partial class Child : Parent
+            {
+                public partial string ChildProp { get; set; }
+                public override string GrandParentProp { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithReactiveAttributeInDifferentNamespace()
+    {
+        var source = @"
+            namespace First
+            {
+                [Reactive]
+                public partial class TestClass
+                {
+                    public partial string Name { get; set; }
+                }
+            }
+
+            namespace Second
+            {
+                [Reactive]
+                public partial class TestClass
+                {
+                    public partial string Name { get; set; }
+                }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithExpressionBodiedProperties()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                private string _name = string.Empty;
+                public string ComputedName => _name.ToUpper();
+                public partial string EditableName { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithTupleProperties()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                public partial (string Name, int Age) PersonInfo { get; set; }
+                public partial (int X, int Y, string Label) Point { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+
+    [Fact]
+    public Task ClassWithInitOnlySetters()
+    {
+        var source = @"
+            [Reactive]
+            public partial class TestClass
+            {
+                public partial string InitOnlyProp { get; init; }
+                public partial string RegularProp { get; set; }
+            }";
+
+        return TestAndVerify(source);
+    }
+    
     private static Task TestAndVerify(
         string source,
         Dictionary<string, string>? analyzerConfigOptions = null,
