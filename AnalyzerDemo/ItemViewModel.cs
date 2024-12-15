5b366b27585773a83f25ce5cf36434588fdc3350
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-12-15 18:32:05 +0100

    Add analyzer demo

---
+++ AnalyzerDemo/ItemViewModel.cs
@@ -0,0 +1,24 @@
+using System.Collections.ObjectModel;
+using ReactiveUI;
+
+namespace AnalyzerDemo;
+
+public class ItemViewModel(string value) : ViewModelBase
+{
+    private ObservableCollection<ItemViewModel>? _items;
+    private string? _value = value;
+
+    public string? Value
+    {
+        get => _value;
+        set => this.RaiseAndSetIfChanged(ref _value, value);
+    }
+
+    public ObservableCollection<ItemViewModel>? Items
+    {
+        get => _items;
+        set => this.RaiseAndSetIfChanged(ref _items, value);
+    }
+
+    public override string ToString() => _value ?? string.Empty;
+}
