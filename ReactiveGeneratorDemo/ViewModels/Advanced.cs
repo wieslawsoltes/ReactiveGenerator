25b97c2c48dc43809986d88fe473a8491dd9d5de
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-01-04 11:50:05 +0100

    Create Advanced.cs

---
+++ ReactiveGeneratorDemo/ViewModels/Advanced.cs
@@ -0,0 +1,10 @@
+namespace ReactiveGeneratorDemo.ViewModels;
+/* TODO:
+[Reactive]
+public partial class Advanced
+{
+    public partial ref int RefValue { get; }
+
+    public partial ref readonly int ReadOnlyRefValue { get; }
+}
+*/
